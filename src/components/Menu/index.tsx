b9c3124f8ae3ff43ca05f98cc55f9f5308c1bbe5
GooseAdmin <admin@goosedefi.com>
2021-02-17 23:55:45 +0800

    update info links

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -17,6 +17,7 @@ const Menu: React.FC = props => {
   return (
     <UikitMenu
       links={links}
+      priceLink="https://www.coingecko.com/en/coins/goose-finance"
       account={account as string}
       login={(connectorId: ConnectorId) => {
         if (connectorId === 'walletconnect') {
