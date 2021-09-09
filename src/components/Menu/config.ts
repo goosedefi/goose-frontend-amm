73a016d44168bc111e8822a0007fda0b104034d3
Martin Abancens <martinabancens@gmail.com>
2021-09-09 12:26:59 -0300

    removing audit endpoints

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -81,16 +81,6 @@ const config: MenuEntry[] = [
     icon: 'GooseIcon',
     href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
   },
-  {
-    label: 'Audit by Hacken',
-    icon: 'AuditIcon',
-    href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
-  },
-  {
-    label: 'Audit by CertiK',
-    icon: 'AuditIcon',
-    href: 'https://certik.org/projects/goose-finance',
-  },
 ]
 
 export default config
